fe62a6cb0ce01c5f3b979480111f737304703eea
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2022-06-10 15:39:53 +0000

    whitespace

--- main.cs
+++ main.cs
@@ -20,8 +20,8 @@ class Program
         double total;
         var size = Console.ReadLine();
         var topping = Console.ReadLine();
-            
-        //Input 
+
+        //Input
         Console.WriteLine("This program calculates the cost of a pizza.");
         Console.WriteLine("");
         Console.Write("Would you like large or extra large? ");
@@ -31,21 +31,32 @@ class Program
         topping = Console.ReadLine();
 
         //Process
-        if (size == large) {
-            top = 6;
-        } else if (size == extraLarge) {
-        }   top = 10;
-
-        if (topping == topping1) {
-            price = 1;
-        } else if (topping == topping2) {
-            price = 1.75;
-        } else if (topping == topping3) {
-            price = 2.5;
-        } else if (topping == topping4) {
-            price = 3.35;
+        if (size == large)
+        {
+          top = 6;
         }
-        
+        else if (size == extraLarge)
+        {
+          top = 10;
+        }
+
+        if (topping == topping1)
+        {
+            price = 1;
+        }
+        else if (topping == topping2)
+        {
+           price = 1.75;
+        }
+        else if (topping == topping3)
+        {
+          price = 2.5;
+        }
+        else if (topping == topping4)
+        {
+          price = 3.35;
+        }
+
         tax = (price + top) * 0.13;
         total = tax + (price + top);
             Console.WriteLine("Your total is: $ " + total);
